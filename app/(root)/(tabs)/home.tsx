105b808de46a5424004f42da087bd4b51d74164b
Kevin Chen <47770053+kevinwchen@users.noreply.github.com>
2024-09-13 16:50:23 +1000

    Add maps and user location

--- app/(root)/(tabs)/home.tsx
+++ app/(root)/(tabs)/home.tsx
@@ -13,6 +13,11 @@ import CustomButton from '@/components/CustomButton';
 import { useClerk } from '@clerk/clerk-expo';
 import RideCard from '@/components/RideCard';
 import { icons, images } from '@/constants';
+import GoogleTextInput from '@/components/GoogleTextInput';
+import Map from '@/components/Map';
+import { useLocationStore } from '@/store';
+import { useEffect, useState } from 'react';
+import * as Location from 'expo-location';
 
 const recentRides = [
   {
@@ -122,11 +127,38 @@ const recentRides = [
 ];
 
 export default function Page() {
+  const { setUserLocation, setDestinationLocation } = useLocationStore();
   const { user } = useUser();
   const { signOut } = useClerk();
   const loading = true;
 
+  const [hasPermissions, setHasPermissions] = useState(false);
+
   const handleSignOut = () => {};
+  const handleDestinationPress = () => {};
+
+  useEffect(() => {
+    const requestLocation = async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        setHasPermissions(false);
+        return;
+      }
+      let location = await Location.getCurrentPositionAsync();
+
+      const address = await Location.reverseGeocodeAsync({
+        latitude: location.coords?.latitude!,
+        longitude: location.coords?.longitude!,
+      });
+
+      setUserLocation({
+        latitude: location.coords.latitude,
+        longitude: location.coords.longitude,
+        address: `${address[0].name}, ${address[0].region}`,
+      });
+    };
+    requestLocation();
+  });
 
   return (
     <SafeAreaView className={'bg-general-500'}>
@@ -174,6 +206,26 @@ export default function Page() {
                 <Image source={icons.out} className={'w-4 h-4'} />
               </TouchableOpacity>
             </View>
+            <GoogleTextInput
+              icon={icons.search}
+              containerStyle={'bg-white shadow-md shadow-neutral-300'}
+              handlePress={handleDestinationPress}
+            />
+            <>
+              <Text className={'text-xl font-JakartaBold mt-5 mb-3'}>
+                Your Current Location
+              </Text>
+              <View
+                className={
+                  'flex flex-row items-center bg-transparent h-[300px]'
+                }
+              >
+                <Map />
+              </View>
+            </>
+            <Text className={'text-xl font-JakartaBold mt-5 mb-3'}>
+              Recent Rides
+            </Text>
           </>
         )}
       />
